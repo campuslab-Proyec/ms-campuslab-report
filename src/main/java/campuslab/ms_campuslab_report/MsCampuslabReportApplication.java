e5c5db9987edfa44f2fe9d853ffac43d0c65c3c6
estefanno monar <estefannomonarsoto2004@gmail.com>
2026-09-11 20:12:20 -0300

    a

---
+++ src/main/java/campuslab/ms_campuslab_report/MsCampuslabReportApplication.java
@@ -0,0 +1,13 @@
+package campuslab.ms_campuslab_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCampuslabReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCampuslabReportApplication.class, args);
+	}
+
+}
