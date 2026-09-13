93a7638a2584da14a14e13c5fcd1f11471b4f4c6
estefanno monar <estefannomonarsoto2004@gmail.com>
2026-09-13 19:24:22 -0300

    cambios

--- src/main/java/com/campuslab/ms_campuslab_report/MsCampuslabReportApplication.java
+++ src/main/java/com/campuslab/ms_campuslab_report/MsCampuslabReportApplication.java
@@ -3,11 +3,12 @@ package com.campuslab.ms_campuslab_report;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MsCampuslabReportApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class MsCampuslabReportApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(MsCampuslabReportApplication.class, args);
 	}
-
 }
